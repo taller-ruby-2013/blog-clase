1696ac83aca815fa0c599f01fbd1c42df9b9a0a3
Ivan Valdes R <monovaldes@gmail.com>
2013-11-24 19:35:23 -0300

    primer commit

---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :commenter, :body, :post_id, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '4d16670d5228aa2c69de64fd86533a831c65c6fa3ac7bef1e6caedc7434aafde1b4fa25246828a8cd0ef4d2a3ca8bf44ddd46854bc6f1041422f93de481ee5ba'
