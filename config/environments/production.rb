754c43468e3c86de19327821c52131c2054bc70d
Ivan Valdes R <mono@centox.(none)>
2014-05-22 18:19:34 -0400

    logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Blog::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.logger = ActiveSupport::Logger.new(File.join(ENV['OPENSHIFT_RUBY_LOG_DIR'], "production.log"))
+
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
